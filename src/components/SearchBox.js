67e60163bd625523a6dc5bd1d43bc4c85d5f3564
Glitch (starter-react-template) <none>
2020-05-25 17:54:39 +0000

    🎎📻 Checkpoint ./src/components/SearchBox.js:15532837/152

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -10,12 +10,12 @@ class SearchBox extends Component {
       }
     }
   
-    getFocus(){
+    onFocus(){
       this.setState({
         isFocused: true
       });
     }
-    getBlur(){
+    onBlur(){
       this.setState({
         isFocused: false
       });
@@ -24,7 +24,7 @@ class SearchBox extends Component {
     render() {
         const {isFocused} = this.state;
         return <div className='SearchBox'>
-                <input onFocus={this.getFocus} onBlur={this.getBlur} type="text" id="input-text"/>
+                <input onFocus={this.onFocus} onBlur={this.onBlur} type="text" id="input-text"/>
                 <img className={classNames({'hide': isFocused === true})} src="https://cdn.glitch.com/3916b47e-07d9-47aa-b61c-db84c74ee7bb%2FEllipse%203.png?v=1590421667794"/>
                 <img className={classNames({'hide': isFocused === true})} src="https://cdn.glitch.com/3916b47e-07d9-47aa-b61c-db84c74ee7bb%2Fsearch%201.png?v=1590422058964"/>    
             </div>;
